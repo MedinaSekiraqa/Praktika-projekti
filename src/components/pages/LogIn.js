b293f913cb3ef4acfa976bafcf05d7f9729d1a9c
MedinaSekiraqa <ms52419@ubt-uni.net>
2022-08-19 17:10:07 +0300

    cards and login

--- src/components/pages/LogIn.js
+++ src/components/pages/LogIn.js
@@ -1,11 +1,22 @@
-import React, {Component} from 'react'
+import { Signup } from "./Signup";
+import contactImg from '../../assets/contact.jpg';
 
-class LogIn extends Component{
-    render(){
-        return(
-            <h1>Meda</h1>
+import './Login.scss'
 
-        );
-    }
+
+function Sign(){
+    return(
+        <div className="container mt-3">
+            <div className="row">
+                <div className="col-md-5">
+                    <Signup/>
+                </div>
+                <div className="col-md-7">
+                    <img className="img-fluid w-1" src={contactImg} alt=""/>
+                </div>
+
+            </div>
+        </div>
+    )
 }
-export default LogIn;
+export default Sign;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { InfoProvider } from './components/pages/context';
 import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <InfoProvider>
+     <React.StrictMode>
     <BrowserRouter>
        <App />
     </BrowserRouter>
 
   </React.StrictMode>
+  </InfoProvider>
+ 
 );
 
 
